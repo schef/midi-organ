190159b8d76736e70961935f52f09b377425cd51
Benjamin Horvat <budikutija@gmail.com>
2014-06-05 13:24:35 +0200

    midi out connector

---
+++ geda/complete_v0.2/midi_out_connector.cmd
@@ -0,0 +1,33 @@
+# Pin name action command file
+
+# Start of element CONN1
+ChangePinName(CONN1, 3, 3)
+ChangePinName(CONN1, 1, 1)
+ChangePinName(CONN1, 2, 2)
+
+# Start of element R2
+ChangePinName(R2, 1, 1)
+ChangePinName(R2, 2, 2)
+
+# Start of element U1
+ChangePinName(U1, 6, 6)
+ChangePinName(U1, 4, 4)
+ChangePinName(U1, 5, 5)
+ChangePinName(U1, 2, 2)
+ChangePinName(U1, 1, 1)
+
+# Start of element D1
+ChangePinName(D1, 2, 2)
+ChangePinName(D1, 1, 1)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
+
+# Start of element CN1
+ChangePinName(CN1, Shield, Shield)
+ChangePinName(CN1, 5, 5)
+ChangePinName(CN1, 4, 4)
+ChangePinName(CN1, 3, 3)
+ChangePinName(CN1, 2, 2)
+ChangePinName(CN1, 1, 1)
